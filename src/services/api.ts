28203c8a021d40325da9883dfaf1131ad9645f60
ABDUMAJIDOV OZODBEK <135005963+abdumajidovOzodbek@users.noreply.github.com>
2025-07-14 23:47:21 +0500

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { User, Channel, ChatMessage } from '../types';
 
-const API_BASE_URL = 'http://localhost:3001/api';
+const API_BASE_URL = 'https://3d1ec4fe-deca-4e92-a36b-c24e61180a71-00-3ux8z4knca40w.pike.replit.dev/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
